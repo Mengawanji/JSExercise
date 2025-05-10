4faac3e26f7e788271db3da8100857faa1744084
Menga wanji <martialwanji@gmail.com>
2025-05-10 17:20:20 +0100

    Second Commit

--- object.js
+++ object.js
@@ -40,5 +40,50 @@ function isEmpty (person) {
 isEmpty(user);
 
 
+
+
+
+// we have an object salaries
+
+// Write a function that takes this object and returns the sum of all salaries
+
+function TotalSalary(a) {
+    let sum = 0;
+    for (let key in a) {
+      sum += a[key];
+    }
+    return sum;
+  }
+  
+  
+  
+  let salaries = {
+    John: 100,
+    Ann: 160,
+    Pete: 130,
+   }
+  
+   console.log(TotalSalary(salaries)); 
+   
+  
+   
+  
+  // Write another function that multiplies all salaries by 3 and returns the new object
+  
+  
+  function multiSalaries(b) {
+    let newSalaries = {};
+  
+    for (let key in b) {
+      if (b.hasOwnProperty(key)) {
+        newSalaries[key] = b[key] * 3;
+      }
+    }
+    return newSalaries;
+  } 
+  
+  console.log(multiSalaries(salaries)); 
+
+
     
 
