0af7084932636ecf2805cd546e0c76841d63584a
Menga wanji <martialwanji@gmail.com>
2025-05-12 04:15:40 +0100

    Second Exercise commit

--- exercise.js
+++ exercise.js
@@ -1,7 +1,32 @@
 //Write a function to reverse a string e.g Hello => olleH
+// ------------------Solution----------------------
 
 function backward(word) {
     return word.split('').reverse().join('');
   }
   
   backward("Hello");
+
+
+
+
+//Log a countdown from 100, countdown should stop at 0 and print a message
+// ------------------Solution----------------------
+
+for (let i = 100; i>=0; i--){
+    console.log(i);
+}
+ console.log("Countdown Endpoint");  
+
+
+
+ //Create a simple function that allows the user to create a profile with
+//name, age and email and prints this in the console;
+// ------------------Solution----------------------
+
+function createProfile (a,b,c) {
+    console.log("User Name: " + a );
+    console.log("Age: " + b );
+    console.log("Email: " + c );
+}
+createProfile("Abel", "35","abel@gmail.com");
